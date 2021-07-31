2386646cb2cc27fd020370235b3228cf73fb2786
Jonny Binns <jonny.binns1@gmail.com>
2021-07-31 13:55:11 +0100

    Finished v1.0, app works but is very basic and not super user friendly. Will work on it more in the future

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # SpotifySaver
-
-Currently under development
-
 Basic program to go through your folders of music and save them to your spotify
 
+This program will only work for music folders structured in the format "~/Artist/Album", this program does not read the tags of the music so folder names need to be accurate
+
+# KNOWN ISSUES
+    Because this program is so simple there are some problems with it that may not be fixed
+* Explicit content, since spotify does include whether an album has explicit songs this program takes the first instance of the album that is returned and saves that. This means there is no control as to whether an explicit version of an album is saved
+* Album names, if the name of the album folder is not the same as the name of the album on spotify it may not be saved, these albums will be included in the error.txt file
+
+
+# Instructions
+1. Make a spotify developer account and create an application (I named mine AlbumSaver)
+2. Go to edit settings and add "http://localhost:8000" as the redirect URL (on first run you will be sent there to enter your spotify credentials)
+3. Clone this repo
+4. In the folder this repo is saved in create a file called config.txt
+5. Go back to the spotify application dashboard, here copy your ClientID and Client Secret to config.txt in the format "(ClientID),(Client Secret)"
+6. Run the application like you would any other python app and sign in when prompted
+
+# Dependencies
+* Spotipy
+
 ### To-Do List
-* Deal with things that arent classed as albums
-* Create series of test folders
-* Deal with requests producing errors
-* Add prompt to ask for music folders path
 * Add error handling to getConfig()
 * Create config.txt if it doesn't exist
-* Create instructions about how to use
-* User testing
--- Saver.py
+++ Saver.py
@@ -12,18 +12,24 @@ def getConfig():
     farr = fstr.split(",")
     return farr
 
-def getAlbumList():
+def getAlbumList(path):
     musicList = []
-    artistList = os.listdir("Music")
+    artistList = os.listdir(path)
     for a in artistList:
-        albumList = os.listdir("Music/" + a)
+        albumList = os.listdir(path + "/" + a)
         for al in albumList:
             musicList.append(a + ":" + al)
     return musicList
 
+def getPath():
+    path = input("Enter the relative path to your music folder, be sure not to have a / as the final character: ")
+    return path
+
 
 def main():
-    albumList = getAlbumList()
+
+    path = getPath()
+    albumList = getAlbumList(path)
 
     configArr = getConfig()
     os.environ["SPOTIPY_CLIENT_ID"] = configArr[0]
@@ -34,19 +40,55 @@ def main():
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
+    notSaved = []
+
     for a in albumList:
         albumArr = a.split(":")
         
-        result = sp.search(albumArr[0], offset=0, type='album', market='GB')
-        #print(json.dumps(result, indent=4, sort_keys=True))
+        result = sp.search(albumArr[0], limit=50, offset=0, type='album', market='GB')
+
+        saved = []
+        notFound = True
+        for i in result['albums']['items']:
+     
+            if a not in saved:
+                if i['name'] == albumArr[1]: 
+                    #print(json.dumps([i], indent=4, sort_keys=True))
+                    result = sp.current_user_saved_albums_add([i['id']])
+                    if result == None:
+                        saved.append(a)
+                        #print(result)
+                        print("Saved: " + albumArr[0] + " - " + albumArr[1])
+                        notFound = False
+
+        if notFound == True:
+            print("FAILED: " + albumArr[0] + " - " + albumArr[1])
+            notSaved.append(a)
+    
+
+    f = open("error.txt", "w")
+    f.close()
+    notSaved = list(dict.fromkeys(notSaved))
+    
+    for a in notSaved:
+        f = open("error.txt", "a")
+        f.write(str(a) + "\n")
+        f.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+    '''
+    result = sp.search('Aphex Twin', limit=50, offset=0, type='album', market='GB')
+    #print(json.dumps(result, indent=4, sort_keys=True))
+    for i in result['albums']['items']:
+        print(i['name'])
+    '''
+
+            # print(json.dumps(result, indent=4, sort_keys=True))
         #print(dir(result))
         #print(result['albums']['items'][0]['id'])
 
         #id = result['albums']['items'][0]['id']
-        for i in result['albums']['items']:
-            if i['name'] == albumArr[1]:
-                result = sp.current_user_saved_albums_add([i['id']])
-                print(result)
-
-if __name__ == '__main__':
-    main()
